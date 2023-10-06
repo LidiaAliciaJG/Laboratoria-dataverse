790c2da2b8689aed4c5e1acd0552532648850b0c
Lidia Alicia <lidiaaliciajg@gmail.com>
2023-10-06 12:17:32 -0600

    primer avance de funciones JS

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,19 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const example = () => {
-  return 'example';
+const btn= document.getElementById("Infantil");
+btn.addEventListener("click", function() {
+  let value="Infantil";
+  console.log("si funciona");
+  filterData(value);
+});
+
+export const filterData = (root, type, value) => {
+  let peliculas = root;
+  const pelisFiltradas = peliculas.filter(pelicula[type]===value);
+  console.log(pelisFiltradas);
+  return pelisFiltradas;
 };
 
-export const anotherExample = () => {
-  return [];
+export const example = () => {
+  return "hola, soy ejemplo";
 };

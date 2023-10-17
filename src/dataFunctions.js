f6d16d1020888115d1c2e2b1b8cad477dc87819b
CarliCG <carlicam22@gmail.com>
2023-10-17 13:04:25 -0500

    ignorar

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,9 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones. FILTRO ORDENAMIENTO 
 import data from './data/dataset.js';
 
-export const filterData = (data, filterBy, value) => {
+export const filterData = (data, value) => {
   function filtrado(data) {
-    return data[filterBy] == value || data.facts[filterBy] == value;
+    return data.type == value;
   };
     return data.filter(filtrado); //filter() requiere una funcion que returne true para revisar el array bajo esa condicion
 };

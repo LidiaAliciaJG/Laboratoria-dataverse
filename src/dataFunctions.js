523a4718fc885f23f5b6c0e0718a160ee05aa4e3
CarliCG <carlicam22@gmail.com>
2023-10-24 16:58:54 -0500

    Primera corección de test

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -3,23 +3,23 @@ import dataset from './data/dataset.js';
 
 export const filterData = (data, filterBy, value) => {
   function filtrado(data) {
-    if (filterBy==="temporality") {
-      if (value=="actual"){
+    if (filterBy === "temporality") {
+      if (value === "actual") {
         return data.facts["temporality"] >= 2010 && data.facts["temporality"] <= 2023;
-      } else if (value=="anterior") {
+      } else if (value === "anterior") {
         return data.facts["temporality"] < 2010 && data.facts["temporality"] >= 1940;
       } else {
         return dataset;
       }
     }
-    return data[filterBy] == value || data.facts[filterBy] == value;
-  };
+    return data[filterBy] === value || data.facts[filterBy] === value;
+  }
   return data.filter(filtrado); //filter() requiere una funcion que returne true para revisar el array bajo esa condicion
 };
 
-const unSort=Array.from(dataset);
+const unSort = Array.from(dataset);
 export const sortData = (data, sortBy, sortOrder) => {
-  if (sortBy==="name") {
+  if (sortBy === "name") {
     if (sortOrder === "asc") {
       // Ordena de la A a la Z basándose en el nombre de la película
       return data.sort((a, b) => a.name.localeCompare(b.name));
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {filterData,sortData} from './dataFunctions.js';
+import { filterData, sortData } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
@@ -7,81 +7,81 @@ console.log(data);
 
 const dataList = document.querySelector("ul");
 const renderData = (dataset) => {
-    dataList.innerHTML= "";
-    dataset.forEach(element => {
-        dataList.appendChild(renderItems(element));
-    });
-}
+  dataList.innerHTML = "";
+  dataset.forEach(element => {
+    dataList.appendChild(renderItems(element));
+  });
+};
 
 const dataNum = document.querySelector("p");
 const renderNum = (num) => {
-  dataNum.innerHTML="Número de películas: "+num;
-}
+  dataNum.innerHTML = "Número de películas: " + num;
+};
 
 const main = () => {
-    renderData(data);
-    renderNum(data.length);
-}
+  renderData(data);
+  renderNum(data.length);
+};
 main();
 
-const original=[...data];
+const original = [...data];
 
 console.log(document.querySelector("ul"));
 
-const filterType= document.getElementById("type-select");
-filterType.addEventListener("change", function() {
-  let filterBy=filterType.name;
-  filterState.filterByType=filterBy;
-  let value=filterType.value;
-  filterState.filterByTypeValue=value;
-  let renderFilter=filterData(data,filterBy,value);
-  if (filterState.filterByDate=="") {
+const filterType = document.getElementById("type-select");
+filterType.addEventListener("change", function () {
+  const filterBy = filterType.name;
+  filterState.filterByType = filterBy;
+  const value = filterType.value;
+  filterState.filterByTypeValue = value;
+  const renderFilter = filterData(data, filterBy, value);
+  if (filterState.filterByDate === "") {
     renderData(renderFilter);
-    renderNum(renderFilter.length)
+    renderNum(renderFilter.length);
   } else {
-    let filterBy=filterState.filterByDate;
-    let value=filterState.filterByDateValue;
-    let renderFilter2=filterData(renderFilter,filterBy,value);
+    const filterBy = filterState.filterByDate;
+    const value = filterState.filterByDateValue;
+    const renderFilter2 = filterData(renderFilter, filterBy, value);
     renderData(renderFilter2);
-    renderNum(renderFilter2.length)
+    renderNum(renderFilter2.length);
   }
 });
 
-const filterDate= document.getElementById("temporality-select");
-filterDate.addEventListener("change", function() {
-  let filterBy=filterDate.name;
-  filterState.filterByDate=filterBy;
-  let value=filterDate.value;
-  filterState.filterByDateValue=value;
-  let renderFilter=filterData(data,filterBy,value);
-  if (filterState.filterByType=="") {
+const filterDate = document.getElementById("temporality-select");
+filterDate.addEventListener("change", function () {
+  const filterBy = filterDate.name;
+  filterState.filterByDate = filterBy;
+  const value = filterDate.value;
+  filterState.filterByDateValue = value;
+  const renderFilter = filterData(data, filterBy, value);
+  if (filterState.filterByType === "") {
     renderData(renderFilter);
-    renderNum(renderFilter.length)
+    renderNum(renderFilter.length);
   } else {
-    let filterBy=filterState.filterByType;
-    let value=filterState.filterByTypeValue;
-    let renderFilter2=filterData(renderFilter,filterBy,value);
+    const filterBy = filterState.filterByType;
+    const value = filterState.filterByTypeValue;
+    const renderFilter2 = filterData(renderFilter, filterBy, value);
     renderData(renderFilter2);
-    renderNum(renderFilter2.length)
+    renderNum(renderFilter2.length);
   }
 });
 
 // Selecciona el elemento select
 const sortName = document.getElementById("sort-select");
-sortName.addEventListener("change", function() { // Agrega un event listener para el cambio en el menú desplegable
-  const sortOrder = sortName.value; // Obtiene el valor seleccionado del menú desplegable
-  filterState.sortOrder=sortOrder;
-  let sortBy = sortName.name;
-  let sortedData = sortData(data,sortBy,sortOrder); // Llama a la función de ordenamiento
-  if (filterState.filterByType=="" && filterState.filterByDate=="") {
+sortName.addEventListener("change", function () {
+  const sortOrder = sortName.value;
+  filterState.sortOrder = sortOrder;
+  const sortBy = sortName.name;
+  const sortedData = sortData(data, sortBy, sortOrder);
+  if (filterState.filterByType === "" && filterState.filterByDate === "") {
     renderData(sortedData);
   } else {
-    let filterByType=filterState.filterByType;
-    let valueType=filterState.filterByTypeValue;
-    let renderFilterType=filterData(data,filterByType,valueType);
-    let filterByDate=filterState.filterByDate;
-    let valueDate=filterState.filterByDateValue;
-    let renderFilterLast=filterData(renderFilterType,filterByDate,valueDate);
+    const filterByType = filterState.filterByType;
+    const valueType = filterState.filterByTypeValue;
+    const renderFilterType = filterData(data, filterByType, valueType);
+    const filterByDate = filterState.filterByDate;
+    const valueDate = filterState.filterByDateValue;
+    const renderFilterLast = filterData(renderFilterType, filterByDate, valueDate);
     renderData(renderFilterLast);
   }
 });
@@ -103,9 +103,9 @@ const filterState = {
 };
 
 const resetFilters = () => {
-  filterState.filterByType="";
-  filterState.filterByDate="";
-  filterState.sortOrder="none";
+  filterState.filterByType = "";
+  filterState.filterByDate = "";
+  filterState.sortOrder = "none";
   filterType.value = "";
   filterDate.value = "";
   sortName.value = "none";

447f97389631772d29ed4fd1a1e81450d8bab1bf
CarliCG <carlicam22@gmail.com>
2023-10-25 12:48:23 -0500

    Cambios en Data del test

--- test/data.js
+++ test/data.js
@@ -1,5 +1,24 @@
 // Aqui puedes agregar los datos que necesites para tus pruebas
 // al menos debe tener uno que se llame "data"
 
-export const data = [];
+export const data = {
+  "id": "escalera-al-cielo",
+  "name": "Escalera al Cielo",
+  "type": "Kdrama",
+  "shortDescription": "Serie coreana de drama y romance.",
+  "description": "Escalera al Cielo es una serie coreana de drama y romance lanzada en 2003. La historia narra el amor y las dificultades de una pareja a lo largo de los años...",
+  "imageUrl": "https://images.justwatch.com/poster/166251016/s718/escalera-al-cielo.jpg",
+  "trailerUrl": "https://www.youtube.com/watch?v=SN61FicHhC0",
+  "facts": {
+    "releaseDate": "3 de diciembre de 2003",
+    "temporality": "2003",
+    "mainActors": ["Choi Ji-woo", "Kwon Sang-woo"],
+    "awards": ["Ninguno"], // Dejar vacío si no ha ganado ningún premio
+    "budget": "No disponible",
+    "criticRating": "75%",
+    "audienceRating": "89%"
+  }
+}
+
+
 
